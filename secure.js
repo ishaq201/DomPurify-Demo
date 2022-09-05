ef277503c147e5d1cd3426364786840257f1a580
ishaq201 <66000368+ishaq201@users.noreply.github.com>
2022-09-05 22:27:45 +0530

    Create secure.js

---
+++ secure.js
@@ -0,0 +1,38 @@
+function sanitizeInput(userInput){
+    return DOMPurify.sanitize(userInput)
+  }
+  
+  function toggleInactiveClass(arrayOfClasses){
+    arrayOfClasses.forEach(el => {
+      document.querySelector(`.${el}`).classList.toggle('inactive'); 
+    })
+  }
+  
+  function handleFormSubmit(e){
+    e.preventDefault();
+    const firstNameInput = document.querySelector('#nameField').value;
+    const bioInput = document.querySelector('#bioField').value;
+    const sanatizedProfileData = sanitizeInput(`
+      <h2>${firstNameInput}</h2>
+      <p>${bioInput}</p>
+    `);
+    const profile = document.querySelector('.profile');
+    profile.innerHTML = sanatizedProfileData;
+    toggleInactiveClass(['profile', 'profile-form']);
+  }
+  
+  document.querySelector('.profile-form').addEventListener('submit', handleFormSubmit);
+  
+  
+  // example payloads
+  
+  // <img src="https://d33wubrfki0l68.cloudfront.net/88c115027b7d4be6a05e82bf3bc2f9d3574e73cc/b20e6/images/cp.svg" onload="alert('you got hacked');"/>
+  // 
+  // <img src="http://unsplash.it/100/asdf" onerror="document.querySelector('body').style.display = 'none';" />
+  // 
+  // <img src="http://unsplash.it/100/asdf" onerror="window.location = 'https://google.com'" />
+  
+  // HTML, SVG and MathML are permitted
+  // , {USE_PROFILES: {html: true}
+  
+  
